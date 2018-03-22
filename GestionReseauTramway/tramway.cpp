1676ed2c01cd9d73180b2bd956249831524278b5
AllKings <36733505+AllKings@users.noreply.github.com>
2018-03-22 14:46:17 +0100

    Update tramway.cpp

--- GestionReseauTramway/tramway.cpp
+++ GestionReseauTramway/tramway.cpp
@@ -1,6 +1,6 @@
 #include "global.h"
 
-tramway::tramway(int num, ligne* li, double vitMax, bool vit, double distTram, double tArret, double distArret, bool sens, arret* arretSuivant): d_numeroTram{num}, d_ligne{li}, d_vitesseMax{vitMax}, d_vitesse{vit}, d_distanceMiniTram{distTram}, d_tempsRestantArret{tArret}, d_distanceArret{distArret}, d_sensDeplacement{sens}, d_arret{arretSuivant}
+tramway::tramway(int num, ligne* li, double vitMax, bool vit, double distTram, double tArret, double distArret, bool sens, arret* arretSuivant, arret* arretPrecedent): d_numeroTram{num}, d_ligne{li}, d_vitesseMax{vitMax}, d_vitesse{vit}, d_distanceMiniTram{distTram}, d_tempsRestantArret{tArret}, d_distanceArret{distArret}, d_sensDeplacement{sens}, d_arretSuiv{arretSuivant},d_arretPrec{arretSuivant}
 {}
 
 pos tramway::GetPosition() const
