dff10ae3294694082adeb9277f76375a0923dc66
Madscientifique <36818408+Madscientifique@users.noreply.github.com>
2018-03-22 13:59:13 +0100

    Add files via upload

--- GestionReseauTramway/listetramways.cpp
+++ GestionReseauTramway/listetramways.cpp
@@ -1,15 +1,35 @@
 #include "global.h"
 
-listeTramways::listeTramways()
+listeTramways::listeTramways():d_tete{nullptr}
 {
     //ctor
 }
-listeTramways::listeTramways(const listeTramways & liste);
+
+listeTramways::listeTramways(const listeTramways & liste):d_tete{nullptr}
+{
+    if(liste.d_tete=nullptr)
+    {
+        return;
+    }
+    else
+    {
+        tramway *c2=liste.d_tete;
+        tramway *c1;
+        tramway *n= new tramway(c2);
+        d_tete=n;
+        c1=n;
+        c2=c2->suiv;
+
+        while()
+        {
+            n= new tramway(c2);
+            c1->suiv=n;
+            c2=c2->suiv;
+        }
+    }
+}
 
 listeTramways::~listeTramways()
 {
     //dtor
 }
-void listeTramways::ajouter(int numTram, double vitesseMax, bool vitesse, double distanceMiniTram, double tempsArret, double distanceArret, bool sensDeplacement, const std::string nomProchainArret);
-void listeTramways::supprimer(int num);
-tramway* listeTramways::chercher(int num) const;
