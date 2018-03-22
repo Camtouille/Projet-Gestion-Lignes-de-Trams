195c404c775cd5cea8d815b4ed58279cbfdfd951
AllKings <36733505+AllKings@users.noreply.github.com>
2018-03-22 15:34:06 +0100

    Update listeligne.h

--- GestionReseauTramway/listeligne.h
+++ GestionReseauTramway/listeligne.h
@@ -7,6 +7,7 @@ class listeligne
     public:
         listeligne();
         ~listeligne();
+        listeligne(const listeligne & liste);
         void ajouter( listearrets &la, listetramways &lt);
         void supprimer( listearrets &la, listetramways &lt);
 
