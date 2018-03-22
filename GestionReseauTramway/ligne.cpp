969032f7947294abd7197b4cb284cdfc8df219c6
AllKings <36733505+AllKings@users.noreply.github.com>
2018-03-22 14:47:53 +0100

    Update ligne.cpp

--- GestionReseauTramway/ligne.cpp
+++ GestionReseauTramway/ligne.cpp
@@ -27,17 +27,3 @@ void ligne::ajouterTram(int numTram, double vitesseMax, bool vitesse, double dis
     tramways.ajouter(int numTram, double vitesseMax, bool vitesse, double distanceMiniTram, double tempsArret, double distanceArret, bool sensDeplacement, const std::string nomProchainArret);
 }
 
-double ligne::distance(const arret & a1, const arret & a2)
-{
-    return (sqrt(abs((a1.position.x*a2.position.x)-(a1.position.y*a2.position.y)));
-}
-
-double ligne::distance(const tramway & t1, const arret & a2)
-{
-    return (sqrt(abs((t1.position.x*a2.position.x)-(t1.position.y*a2.position.y)));
-}
-
-double ligne::distance(const tramway & a1, const tramway & a2)
-{
-    return (sqrt(abs((t1.position.x*t2.position.x)-(t1.position.y*t2.position.y)));
-}
