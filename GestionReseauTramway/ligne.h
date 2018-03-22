4ae09aa59c74be3502088e71608cf3ddc39da6da
Madscientifique <36818408+Madscientifique@users.noreply.github.com>
2018-03-22 13:28:41 +0100

    Add files via upload

---
+++ GestionReseauTramway/ligne.h
@@ -0,0 +1,22 @@
+#ifndef LIGNE_H
+#define LIGNE_H
+class arret;
+class listeArrets;
+class listeTramways;
+
+class ligne
+{
+	public:
+		ligne();
+		ligne(const ligne & l);
+		void ajouterArret(std::string nom, int dureeArret, pos position);
+		void ajouterTram(int numTram, double vitesseMax, bool vitesse, double distanceMiniTram, double tempsArret, double distanceArret, bool sensDeplacement, const std::string nomProchainArret);
+		double distance(const arret & a1, const arret & a2);
+		double distance(const tramway & t, const arret & a);
+		double distance(const tramway & t1, const tramway & t2);
+	private:
+		listeArrets arrets;
+		listeTramways tramways;
+};
+
+#endif // LIGNE_H
