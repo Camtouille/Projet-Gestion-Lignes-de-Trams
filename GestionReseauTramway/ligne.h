6f607af254659514bb16fb3ed5de86105b4e3214
AllKings <36733505+AllKings@users.noreply.github.com>
2018-03-22 14:48:50 +0100

    Update ligne.h

--- GestionReseauTramway/ligne.h
+++ GestionReseauTramway/ligne.h
@@ -12,9 +12,7 @@ class ligne
 		std::vector<pos> GetPosArret();
 		void ajouterArret(std::string nom, int dureeArret, pos position);
 		void ajouterTram(int numTram, double vitesseMax, bool vitesse, double distanceMiniTram, double tempsArret, double distanceArret, bool sensDeplacement, const std::string nomProchainArret);
-		double distance(const arret & a1, const arret & a2);
-		double distance(const tramway & t1, const arret & a2);
-		double distance(const tramway & t1, const tramway & t2);
+		
 	private:
 		listeArrets arrets;
 		listeTramways tramways;
