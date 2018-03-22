b066cd780d7ead67d555414fadf6772ba2f2c3be
AllKings <36733505+AllKings@users.noreply.github.com>
2018-03-22 13:17:42 +0100

    Update listetramways.cpp

--- GestionReseauTramway/listetramways.cpp
+++ GestionReseauTramway/listetramways.cpp
@@ -4,8 +4,12 @@ listeTramways::listeTramways()
 {
     //ctor
 }
+listeTramways(const listeTramways & liste);
 
 listeTramways::~listeTramways()
 {
     //dtor
 }
+void ajouter(int numTram, double vitesseMax, bool vitesse, double distanceMiniTram, double tempsArret, double distanceArret, bool sensDeplacement, const std::string nomProchainArret);
+void supprimer(int num);
+tramway* chercher(int num) const;
