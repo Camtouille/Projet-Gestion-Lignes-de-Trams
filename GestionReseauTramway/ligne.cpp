f6899fff3fbf00ba83fb8e4e0cda1002f8187906
Madscientifique <36818408+Madscientifique@users.noreply.github.com>
2018-03-22 13:30:19 +0100

    Add files via upload

--- GestionReseauTramway/ligne.cpp
+++ GestionReseauTramway/ligne.cpp
@@ -16,7 +16,17 @@ ligne::~ligne()
     //dtor
 }
 
-double ligne::distanceEntreArret(const arret & a1, const arret & a2)
+double ligne::distance(const arret & a1, const arret & a2)
 {
-    return (sqrt(abs((a1.position.x*a1.position.x)-(a1.position.y*a1.position.y)));
+    return (sqrt(abs((a1.position.x*a2.position.x)-(a1.position.y*a2.position.y)));
+}
+
+double ligne::distance(const tramway & t1, const arret & a2)
+{
+    return (sqrt(abs((t1.position.x*a2.position.x)-(t1.position.y*a2.position.y)));
+}
+
+double ligne::distance(const tramway & a1, const tramway & a2)
+{
+    return (sqrt(abs((t1.position.x*t2.position.x)-(t1.position.y*t2.position.y)));
 }
